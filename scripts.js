29b36298d76d1c78fc39ce855cbaf27930f8c835
mohamed hassan <kino52965@gmail.com>
2025-03-15 01:23:06 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const featuredComics = [
         { img: "imags comics/swcapa2025001_dc11.jpeg", title: "Sam Wilson, Captain America (2025)", link: "sam.html" },
         { img: "imags comics/clean.jpg", title: "Daredevil Facsimile Edition (2025)", link: "dare.html" },
-        { img: "imags comics/clean (1).jpg", title: "Red Hulk (2025)", link: "red.html" }
+        { img: "imags comics/RED HULK.jpg", title: "Red Hulk (2025)", link: "red.html" }
     ];
     
     const moreComics = [
@@ -20,6 +20,12 @@ document.addEventListener("DOMContentLoaded", function() {
         { img: "imags comics/doom.jpg", title: "One World Under Doom (2025) #1", file: "https://www.mediafire.com/file/rfvszrb3cz76e75/oneworlddoom.cbz/file" },
         { img: "imags comics/marvelholiday.jpg", title: "MARVEL HOLIDAY TALES TO ASTONISH (2024) #1", file: "https://www.mediafire.com/file/trllb5ok7oxp0af/Marvel_Holiday_Tales_To_Astonish_001_%25282025%2529_%2528Digital%2529_%2528Shan-Empire%2529.cbz/file" },
         { img: "imags comics/moonknight12.jpg", title: "Moon Knight – Fist of Khonshu (2024) #0", file: "https://www.mediafire.com/file/wdaycqdewwjkk6r/Moon_Knight_-_Fist_Of_Khonshu_000_%25282024%2529_%2528Digital%2529_%2528Shan-Empire%2529.cbz/file" },
+        { img: "imags comics/Iron Man (2024).jpg", title: "Iron Man (2024)#1", file: "https://www.mediafire.com/file/6kw9b68zu6u2c4y/Iron_Man_001_%25282024%2529_%2528Digital%2529_%2528Shan-Empire%2529.cbz/file" },
+        { img: "imags comics/Marvel Rivals Infinity Comic.jpg", title: "Marvel Rivals Infinity Comic (2024) #1", file: "https://www.mediafire.com/file/6m1pebamnbu03e6/Marvel_Rivals_-_Infinity_Comic_001_%25282024%2529_%2528digital-mobile-Empire%2529.cbz/file" },
+        { img: "imags comics/DOCTOR DOOM & ROCKET RACCOON.jpg", title: "DOCTOR DOOM & ROCKET RACCOON (2025) #1", file: "https://www.mediafire.com/file/vs5713dhroxfza3/Doctor_Doom_%2526_Rocket_Raccoon_001_%25282025%2529_%2528Digital%2529_%2528Shan-Empire%2529.cbz/file" },
+        { img: "imags comics/bloodhunt.jpg", title: "Blood Hunt (2024) #1", file: "https://www.mediafire.com/file/o7ax4cft7g5o8a5/Blood_Hunt_-_Red_Band_001_%25282024%2529_%25284_covers%2529_%2528Digital%252Bscan%2529_%2528dekabro-Empire%2529.cbz/file" },
+        { img: "imags comics/Aliens Vs. Avengers.jpg", title: "Aliens Vs. Avengers (2024) #1", file: "https://www.mediafire.com/file/3y5nqfz8yvgae0i/Aliens_vs._Avengers_001_%25282024%2529_%2528Digital%2529_%2528Shan-Empire%2529.cbz/file" },
+        { img: "imags comics/marvalzomb.jpg", title: "Marvel Zombies: Dawn of Decay (2024) #1", file: "https://www.mediafire.com/file/l10zfryoglmxsbd/Marvel_Zombies_-_Dawn_Of_Decay_001_%25282024%2529_%2528Digital%2529_%2528Shan-Empire%2529.cbz/file" },
         { img: "imags comics/deadpool2.jpg", title: "DEADPOOL/WOLVERINE: WEAPON X-TRACTION (2029) #1", file: "https://www.mediafire.com/file/9se2iwydgo95wdc/Deadpool-Wolverine_-_Weapon_X-Traction_001_%25282025%2529_%2528Digital%2529_%2528Kileko-Empire%2529.cbz/file" }
     ];
 
